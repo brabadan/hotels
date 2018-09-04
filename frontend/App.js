1a086b263e63fa977052176624be40ffba90a881
Rabadan Bakhmudov <brabadan@yandex.ru>
2018-09-04 21:22:24 +0300

    Created Vue Project in directory front_vue

---
+++ frontend/App.js
@@ -0,0 +1,8 @@
+import TheHeader from './TheHeader';
+
+var app = new Vue( {
+    el: "#app",
+    data: {
+        text: "Hello kuku!"
+    }
+});
---
+++ frontend/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Header!!!
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TheHeader",
+    }
+</script>
+
+<style scoped>
+
+</style>
