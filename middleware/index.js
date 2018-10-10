90e8924eb7aa336255577e11de13c55b234fe87f
Rabadan Bakhmudov <brabadan@yandex.ru>
2018-10-10 16:33:21 +0300

    express.static pass -> front_vue/dist

--- middleware/index.js
+++ middleware/index.js
@@ -3,5 +3,5 @@ module.exports = function (app, express) {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
     app.use('/qwest', express.static('public/qwest'));
-    app.use(express.static('./front_vue/dist'));
+    app.use(express.static('front_vue/dist'));
 };
