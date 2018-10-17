7ae5ae802b923322abcf7941ea8b1f27e3d9d81f
Rabadan Bakhmudov <brabadan@yandex.ru>
2018-10-17 11:00:40 +0300

    комменты в app.js

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 const app = require('express')();
-// const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const config = require('./config/index');
 // Для функционирования на OpenShift
 const server_port = process.env.OPENSHIFT_NODEJS_PORT || config.port;
-const server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
+// подключаем Статику и настройки
 require('./middleware')(app);
 
-// MongoClient.connect(config.db.url, { useNewUrlParser: true }, (err, client) => {
-
+// соединяем мангуст с БД
 mongoose.connect(config.db.url);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 
+// при открытии соединения мангустом
 db.once('open', function () {
-    // const db = client.db('hotels');
-
+    // Включаем сессии
     app.use(session({
         secret: config.session.secret,
         cookie: config.session.cookie,
@@ -26,11 +24,11 @@ db.once('open', function () {
         resave: false,
         store: new MongoStore({mongooseConnection: db})
     }));
-
     // Включаем Аутентификацию
     require('./middleware/Auth')(app, db);
-
+    // Включаем маршруты
     require('./app/routes')(app, mongoose);
+    // Запускаем сервер
     app.listen(server_port, () => {
         console.log(new Date(), `listening on ${server_port} port`);
     });
