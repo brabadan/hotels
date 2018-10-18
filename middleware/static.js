edd5d6a3288aa42212f7da90ddb34b5b23a910f7
Rabadan Bakhmudov <brabadan@yandex.ru>
2018-10-18 09:53:58 +0300

    hotels fixed

--- middleware/static.js
+++ middleware/static.js
@@ -1,4 +1,4 @@
-const config = require('../config/index');
+// const config = require('../config/index');
 const express = require('express');
 
 module.exports = function (app) {
@@ -6,5 +6,6 @@ module.exports = function (app) {
     app.use('/pyatnashki', express.static('public/pyatnashki'));
     app.use('/kubik', express.static('public/kubik'));
     app.use('/qwest', express.static('public/qwest'));
-    app.use(config.app_path, express.static('public/hotels'));
+    // app.use(config.app_path, express.static('public/hotels'));
+    app.use('/hotels', express.static('public/hotels'));
 };
